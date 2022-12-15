1f20f18da64917cf9d5aac17d19f7bc3b6930d3c
vaibhav493 <itsmevaibhavwaghmare7666@gmail.com>
2022-12-15 10:06:39 +0530

    adding day2 again

---
+++ longing-veil/src/Context/ProductContext.js
@@ -0,0 +1,11 @@
+
+import { useRadioGroupContext } from "@chakra-ui/react";
+import { useState , createContext } from "react";
+
+
+export default function ProductContext(){
+
+    return(
+        <div></div>
+    )
+}
